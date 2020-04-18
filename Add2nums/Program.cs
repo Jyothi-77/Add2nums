01ab987470c010e9816502985576052d68b4addf
jyothile501 <jyothile501@gmail.com>
2020-04-18 14:03:39 +0530

    Add project files.

---
+++ Add2nums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Add2nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10;
+            int b=20;
+            Console.WriteLine("enter a value:", +a);
+            Console.WriteLine("enter b value:", +b);
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
